9b9d1e88f29cd9335f39920a34740a2919cd7508
J0J0 T <jojo@peek-a-boo.at>
2018-10-09 08:38:34 +0200

    basic online release search functionality

--- cli.py
+++ cli.py
@@ -11,6 +11,7 @@ import pprint
 import discogs_client.exceptions as errors
 import requests.exceptions as reqerrors
 import urllib3.exceptions as urlerrors
+import pprint as pp
 
 # argparser init
 def argparser(argv):
@@ -58,8 +59,8 @@ def search_release_online(discogs, id_or_title):
                 release = discogs.release(id_or_title)
                 return '|'+str(release.id)+'|'+ str(release.title)+'|'
             else:
-                releases = discogs.search(id_or_title)
-                return releases.type
+                releases = discogs.search(id_or_title, type='release')
+                return releases
         except errors.HTTPError as HtErr:
             log.error("%s", HtErr)
         except urlerrors.NewConnectionError as ConnErr:
@@ -94,6 +95,10 @@ def search_release_offline(dbconn, id_or_title):
 def main():
 	# SETUP / INIT
     args = argparser(sys.argv)
+    #if hasattr(args, ''):
+    #if len(args)==1:
+    #    print_help('No args')
+
     conn = db.create_conn("/Users/jojo/git/discodos/discobase.db")
 
     # DEBUG stuff
@@ -108,7 +113,7 @@ def main():
         me = d.identity()
         online=True
     except Exception:
-        log.error("Error connecting to Discogs API, let's stay offline!\n")
+        log.error("connecting to Discogs API, let's stay offline!\n")
         online=False
 
     if hasattr(args, 'release_cmd'):
@@ -116,15 +121,37 @@ def main():
             print_help("Showing all releases, this is gonna take some time")
             db.all_releases(conn)
         else:
+            db_releases = db.all_releases(conn)
             for list_element in args.release_cmd:
                 if online:
                     print_help('Searching Discogs for Release ID or Title \"'+list_element+'\"')
-                    print_help(search_release_online(d, list_element))
+                    search_results = search_release_online(d, list_element)
+                    print_help("Found "+str(search_results.pages )+" page(s) of results!")
+                    for result_item in search_results:
+                        #if result_item.id in me.collection_folders[0].releases:
+                        print_help("Checking " + str(result_item.id))
+                        for dbr in db_releases:
+                            #print_help(dbr[0])
+                            if result_item.id == dbr[0]:
+                                 print_help("Good, first matching record in your collection is:")
+                                 result ='| '+result_item.artists[0].name+' | '+result_item.title
+                                 result+=' | '+str(result_item.labels[0])+' |\n| '
+                                 result+=result_item.country+' | '+str(result_item.year)+' | '
+                                 result+=str(result_item.formats[0]['descriptions'][0])
+                                 result+=', '+str(result_item.formats[0]['descriptions'][1])
+                                 # string build done, now print
+                                 print_help(result)
+                                 tracklist = result_item.tracklist
+                                 for track in tracklist:
+                                    print(track)
+                                 break
+                        if result_item.id == dbr[0]:
+                            break
                 else:
                     print_help('Searching offline DB for \"' + list_element +'\"')
                     print_help(search_release_offline(conn, list_element))
     elif hasattr(args, 'track_cmd'):
-        log.debug("we are in track_cmd branch")
+        log.debug("We are in track_cmd branch")
 
 
 
@@ -136,4 +163,9 @@ def main():
         log.debug("DB closed.")
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        log.error('Program interrupted!')
+    #finally:
+        #log.shutdown()
