34738fb097d2f3452eaddf87bf82390ff7063a9b
J0J0 T <jojo@peek-a-boo.at>
2018-11-10 03:26:38 +0100

    forgot to add utils.py to git
    also: quickfix error in online search description pretty_print (index)

---
+++ discodos/utils.py
@@ -0,0 +1,30 @@
+from discodos import log
+import datetime
+
+# util: checks for numbers
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+    except TypeError:
+        return False
+
+# util: print a UI message
+def print_help(message):
+    print(''+str(message)+'\n')
+
+# util: ask user for some string
+def ask_user(text=""):
+    return input(text)
+
+# util: Discogs: stay in 60/min rate limit
+def rate_limit_slow_downer(d_obj, remaining=10, sleep=2):
+    if int(d_obj._fetcher.rate_limit_remaining) < remaining:
+        log.info("Discogs request rate limit is about to exceed,\
+                  let's wait a bit: %s\n",
+                     d_obj._fetcher.rate_limit_remaining)
+        time.sleep(sleep)
+
+
