1135af918c6805a59a643edf2b6c95d834bc7234
J0J0 T <jojo@peek-a-boo.at>
2019-06-03 08:04:25 +0200

    add d_artist and in_d_collection fields to create_db_tables()

--- setup.py
+++ setup.py
@@ -50,6 +50,8 @@ def create_db_tables(_conn):
                                        discogs_id INTEGER PRIMARY KEY ON CONFLICT REPLACE,
                                        discogs_title TEXT NOT NULL,
                                        import_timestamp TEXT
+                                       d_artist TEXT,
+                                     in_d_collection INTEGER,
                                         ); """
     sql_create_mix_table = """ CREATE TABLE IF NOT EXISTS mix (
                                     mix_id INTEGER PRIMARY KEY,
