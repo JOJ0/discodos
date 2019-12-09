f42e822c4aaf114bba903eb5eedc15d169638055
J0J0 T <jojo@peek-a-boo.at>
2019-12-09 22:47:23 +0100

    utils.py: add read_yaml function

--- discodos/utils.py
+++ discodos/utils.py
@@ -1,5 +1,6 @@
 from discodos import log
 import time
+import yaml
 
 # util: checks for numbers
 def is_number(s):
@@ -26,3 +27,26 @@ def rate_limit_slow_downer(d_obj, remaining=10, sleep=2):
                   let's wait a bit: %s\n",
                      d_obj._fetcher.rate_limit_remaining)
         time.sleep(sleep)
+
+# read yaml
+def read_yaml(yamlfile):
+    """expects path/file"""
+    try:
+        with open(yamlfile, "r") as fyamlfile:
+            return yaml.load(fyamlfile)
+    except IOError as errio:
+        log.warn("Can't find %s \n\n", yamlfile)
+        raise errio
+        raise SystemExit(3)
+    except yaml.parser.ParserError as errparse:
+        log.error("ParserError in %s \n\n", yamlfile)
+        #raise errparse
+        raise SystemExit(3)
+    except yaml.scanner.ScannerError as errscan:
+        log.error("ScannerError in %s \n\n", yamlfile)
+        #raise errscan
+        raise SystemExit(3)
+    except Exception as err:
+        log.error(" trying to load %s \n\n", yamlfile)
+        #raise err
+        raise SystemExit(3)
