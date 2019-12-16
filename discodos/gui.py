9f9dde527eaf78b5730f16a76accc6312e54886b
schobernoise <reach@fabianschober.com>
2019-12-16 23:08:56 +0100

    Main Frame running, first table up

--- discodos/gui.py
+++ discodos/gui.py
@@ -5,14 +5,12 @@ from tkinter import ttk
 
 class main_frame():
     def __init__(self):
-
-
         self.main_win = tk.Tk()                           
         self.main_win.geometry("860x500")                # Fixed size for now, maybe scalable later
         self.main_win.resizable(False, False)    
         
     
-        self.win.title("Language Construction Tool") # EDIT: Add relevant Information to title, like Titles in Mix etc
+        self.main_win.title("Language Construction Tool") # EDIT: Add relevant Information to title, like Titles in Mix etc
 
         # Create all Widgets, outsourced to its own function
         self.create_widgets()
@@ -21,8 +19,8 @@ class main_frame():
                     
     # Exit GUI cleanly
     def _quit(self):
-        self.win.quit()
-        self.win.destroy()
+        self.main_win.quit()
+        self.main_win.destroy()
         exit() 
 
 
@@ -30,4 +28,22 @@ class main_frame():
     # CREATE WIDGETS
 
     def create_widgets(self):
-        pass
+        self.mix_list = ttk.Treeview(self.main_win )
+        self.mix_list.grid(row=0, column=0, sticky="ns")
+
+        self.mix_list["columns"]=("one","two","three")
+        self.mix_list.column("#0",  minwidth=4, stretch=tk.NO)
+        self.mix_list.column("name", minwidth=80)
+        self.mix_list.column("played",  minwidth=20, stretch=tk.NO)
+        self.mix_list.column("venue", width=80, minwidth=25, stretch=tk.NO)
+        self.mix_list.column("created", width=80, minwidth=20, stretch=tk.NO)
+        self.mix_list.column("updated", width=80, minwidth=20, stretch=tk.NO)
+
+        self.mix_list.heading("#0",text="Mix #",anchor=tk.W)
+        self.mix_list.heading("name", text="Name", anchor=tk.W)
+        self.mix_list.heading("played", text="Played",anchor=tk.W)
+        self.mix_list.heading("venue", text="Venue",anchor=tk.W)
+        self.mix_list.heading("created", text="Created",anchor=tk.W)
+        self.mix_list.heading("updated", text="Updated",anchor=tk.W)
+
+    
--- start_gui.py
+++ start_gui.py
@@ -1,6 +1,6 @@
-from discodos.gui import *
+from discodos.gui import main_frame
 import tkinter as tk
 
 
-main_gui = main_frame
+main_gui = main_frame()
 main_gui.main_win.mainloop()
