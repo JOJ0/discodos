552c8f97accc3e7411f6fc968c47edcc0f94ba23
J0J0 T <jojo@peek-a-boo.at>
2020-01-23 07:47:23 +0100

    add folders/files for basic model testing

---
+++ tests/models/test_mix.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class TestBasic(unittest.TestCase):
+    def setUp(self):
+        # Load test data
+        self.app = App(database='fixtures/test_basic.json')
+
+    def test_customer_count(self):
+        self.assertEqual(len(self.app.customers), 100)
+
+    def test_existence_of_customer(self):
+        customer = self.app.get_customer(id=10)
+        self.assertEqual(customer.name, "Org XYZ")
+        self.assertEqual(customer.address, "10 Red Road, Reading")
+
+if __name__ == '__main__':
+    unittest.main()
