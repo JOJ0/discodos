e512c434f8062ec7e265ed1f617df12b5574cb7e
J0J0 T <jojo@peek-a-boo.at>
2020-01-24 08:29:32 +0100

    tests: mv to discobase_empty.db and copy on test start
    * empty db is copied to tests dir and deleted when all tests finished
    * delete old test-db from repo
    
    also:
    * add test_mix_create - no useful asserts for now - mix.create
      method should be fixed before (still uses deprecated db.function)

--- tests/test_mix.py
+++ tests/test_mix.py
@@ -1,26 +1,42 @@
 import unittest
+from shutil import copy2
+from os import remove
 from discodos.models import *
 from discodos.utils import *
 
 
 class TestMix(unittest.TestCase):
-    def setUp(self):
-        log.handlers[0].setLevel("DEBUG") # handler 0 is the console handler
-        conf = Config()
-        db_path = "{}/tests/fixtures/discobase.db ".format(conf.discodos_root)
-        log.warning("db path: {}".format(db_path))
-        # instantiate the Mix model class (empty, mix 123 = non-existent)
-        self.mix = Mix(False, 123, db_path) 
-        log.info("TestMix.setUp: done")
+    @classmethod
+    def setUpClass(self):
+        log.handlers[0].setLevel("INFO") # handler 0 is the console handler
+        conf = Config() # doesn't get path of test-db, so...
+        empty_db_path = conf.discodos_root / 'tests' / 'fixtures' / 'discobase_empty.db'
+        self.db_path = conf.discodos_root / 'tests' / 'discobase.db'
+        print('TestMix.setUpClass: test-db: {}'.format(copy2(empty_db_path, self.db_path)))
+        print("TestMix.setUpClass: done\n")
 
     def test_mix_non_existent(self):
+        print("TestMix.mix_non_existent: BEGIN")
+        # instantiate the Mix model class (empty, mix 123 = non-existent)
+        self.mix = Mix(False, 123, self.db_path) 
         self.assertFalse(self.mix.id_existing)
-        log.info("TestMix.mix_non_existent: done")
+        print("TestMix.mix_non_existent: DONE\n")
 
-    #def test_existence_of_customer(self):
-    #    customer = self.app.get_customer(id=10)
+    def test_mix_create(self):
+        print("TestMix.mix_create: BEGIN")
+        self.mix = Mix(False, 123, self.db_path) 
+        self.mix.create("2020-01-01", "test venue", "test mix 1")
+        #self.assertEqual(self.mix.id, 1)
+        #self.assertTrue(self.mix.id_existing)
+        print("TestMix.mix_create: DONE\n")
+
+    #def test_existence_of_mix(self):
     #    self.assertEqual(customer.name, "Org XYZ")
-    #    self.assertEqual(customer.address, "10 Red Road, Reading")
+
+    @classmethod
+    def tearDownClass(self):
+        os.remove(self.db_path)
+        print("\nTestMix.teardownClass: done")
 
 if __name__ == '__main__':
     unittest.main()
