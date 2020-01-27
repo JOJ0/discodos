1c12017db951b2ff8d5fe8eaff360ac3a0f4af36
J0J0 T <jojo@peek-a-boo.at>
2020-01-27 07:45:59 +0100

    tests: rename all methods - remove _mix_

--- tests/test_mix.py
+++ tests/test_mix.py
@@ -16,15 +16,15 @@ class TestMix(unittest.TestCase):
         print('TestMix.setUpClass: test-db: {}'.format(copy2(empty_db_path, self.db_path)))
         print("TestMix.setUpClass: done\n")
 
-    def test_mix_non_existent(self):
-        print("\nTestMix.mix_non_existent: BEGIN")
+    def test_non_existent(self):
+        print("\nTestMix.non_existent: BEGIN")
         # instantiate the Mix model class (empty, mix 123 = non-existent)
         self.mix = Mix(False, 123, self.db_path) 
         self.assertFalse(self.mix.id_existing)
-        print("TestMix.mix_non_existent: DONE\n")
+        print("TestMix.non_existent: DONE\n")
 
-    def test_mix_create(self):
-        print("\nTestMix.mix_create: BEGIN")
+    def test_create(self):
+        print("\nTestMix.create: BEGIN")
         self.mix = Mix(False, 123, self.db_path) 
         db_return = self.mix.create("2020-01-01", "test venue", "test mix 1")
         self.assertEqual(db_return, 1)
@@ -33,10 +33,10 @@ class TestMix(unittest.TestCase):
         self.assertEqual(self.mix.name, "test mix 1")
         self.assertEqual(self.mix.venue, "test venue")
         self.assertEqual(self.mix.played, "2020-01-01")
-        print("TestMix.mix_create: DONE\n")
+        print("TestMix.create: DONE\n")
 
-    def test_mix_delete(self):
-        print("\nTestMix.mix_delete: BEGIN")
+    def test_delete(self):
+        print("\nTestMix.delete: BEGIN")
         self.mix = Mix(False, 124, self.db_path)
         db_return = self.mix.delete()
         self.assertEqual(db_return, 1)
@@ -45,10 +45,10 @@ class TestMix(unittest.TestCase):
         # self.mix.name is kept for user help output
         self.assertFalse(self.mix.venue)
         self.assertFalse(self.mix.played)
-        print("TestMix.mix_delete: DONE\n")
+        print("TestMix.delete: DONE\n")
 
-    def test_mix_get_one_mix_track(self):
-        print("\nTestMix.mix_get_one_mix_track: BEGIN")
+    def test_get_one_mix_track(self):
+        print("\nTestMix.get_one_mix_track: BEGIN")
         self.mix = Mix(False, 125, self.db_path)
         db_return = self.mix.get_one_mix_track(2) # get track #2
         #print(db_return.keys())
@@ -67,10 +67,10 @@ class TestMix(unittest.TestCase):
         self.assertEqual(db_return["notes"], "test note B2")
         # how would I test this? db_return["mix_track_id"], "")
         self.assertEqual(db_return["d_release_id"], 123456)
-        print("TestMix.mix_get_one_mix_track: DONE\n")
+        print("TestMix.get_one_mix_track: DONE\n")
 
-    def test_mix_get_tracks_of_one_mix(self):
-        print("\nTestMix.mix_get_tracks_of_one_mix: BEGIN")
+    def test_get_tracks_of_one_mix(self):
+        print("\nTestMix.get_tracks_of_one_mix: BEGIN")
         self.mix = Mix(False, 125, self.db_path)
         db_return = self.mix.get_tracks_of_one_mix()
         self.assertEqual(len(db_return), 2)
@@ -80,7 +80,7 @@ class TestMix(unittest.TestCase):
         self.assertEqual(db_return[1]["mix_id"], 125)
         self.assertEqual(db_return[1]["d_release_id"], 123456)
         self.assertEqual(db_return[1]["d_track_no"], "B2")
-        print("TestMix.mix_get_tracks_of_one_mix: DONE\n")
+        print("TestMix.get_tracks_of_one_mix: DONE\n")
 
     def test_get_mix_info(self):
         print("\nTestMix.get_mix_info: BEGIN")
