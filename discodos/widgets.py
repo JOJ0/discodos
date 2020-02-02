556cee3ccdd9166ee71bb3d0abcda4aaebfaaf34
schobernoise <reach@fabianschober.com>
2020-02-02 22:27:33 +0100

    Track Insertion corrected, conitnued with windows

--- discodos/widgets.py
+++ discodos/widgets.py
@@ -25,7 +25,7 @@ class widget_frame():
 
 class edit_mix_view(widget_frame):
     def __init__(self, parent, mix_data, conn):
-        self.title = "Edit Mix Info"
+        self.title = "Edit Mix"
         super().__init__(parent, self.title)
         self.mix_data = mix_data
         self.view_mix_content()
@@ -75,8 +75,8 @@ class edit_mix_view(widget_frame):
         for i in range(5,9):
             self.mix_info_frame.rowconfigure(i, weight=1)
 
-        self.update_collection = tk.Button(self.mix_info_frame, text="Update Collection")
-        self.update_collection.grid(row=8, column=0, sticky="s")
+        self.update_mix_btn = tk.Button(self.mix_info_frame, text="Update Mix", command=update_mix)
+        self.update_mix_btn.grid(row=8, column=0, sticky="s")
 
 
         #############################################
@@ -96,8 +96,6 @@ class edit_mix_view(widget_frame):
         self.coll_list.heading("id", text="Discogs ID",anchor=tk.W)
 
 
-
-        # TODO: fetch discogs track data
 
 
         ###############################################
@@ -124,26 +122,175 @@ class edit_mix_view(widget_frame):
         for i, row in enumerate(self.all_releases):
             self.coll_list.insert("" , i, text=row["discogs_title"], values=(row["d_artist"], row["discogs_id"]))
 
+    def update_mix(self):
+        pass
+
+
+
     def add_track_to_mix(self):
+
         pass
 
         
+class add_mix_view(widget_frame):
+    def __init__(self, parent, conn):
+        self.title = "Add Mix"
+        super().__init__(parent, self.title)
+        self.view_mix_content()
+        self.conn = conn
+        self.insert_track_pool()
+        
+    
+    def view_mix_content(self):
 
+        self.mix_info_frame = tk.LabelFrame(self.edit_win, text="Mix Info")
+        self.pool_frame = tk.LabelFrame(self.edit_win, text="Discogs Track Pool")
+        self.buttons_frame = tk.Frame(self.edit_win)
+
+        #############################################
+        # Mix Info View
+        #############################################
+
+
+        tk.Label(self.mix_info_frame, text="Name").grid(row=0, column=0, sticky="w")
+        self.name_entry = tk.Entry(self.mix_info_frame, width=30)
+        self.name_entry.grid(row=0, column=1, sticky="w")
+
+
+        tk.Label(self.mix_info_frame, text="Played").grid(row=1, column=0, sticky="w")
+        self.played_entry = tk.Entry(self.mix_info_frame, width=30)
+        self.played_entry.grid(row=1, column=1, sticky="w")
+
+        tk.Label(self.mix_info_frame, text="Venue").grid(row=2, column=0, sticky="w")
+        self.venue_entry = tk.Entry(self.mix_info_frame, width=30)
+        self.venue_entry.grid(row=2, column=1, sticky="w")
+
+        tk.Label(self.mix_info_frame, text="Created").grid(row=3, column=0, sticky="w")
+        tk.Label(self.mix_info_frame, text="NOW??").grid(row=3, column=1, sticky="w")
+
+        tk.Label(self.mix_info_frame, text="Last Updated").grid(row=4, column=0, sticky="w")
+        tk.Label(self.mix_info_frame, text="").grid(row=4, column=1, sticky="w")
+
+        for i in range(5,9):
+            self.mix_info_frame.rowconfigure(i, weight=1)
+
+        self.update_collection_btn = tk.Button(self.mix_info_frame, text="Save new Mix", command=self.save_mix)
+        self.update_collection_btn.grid(row=8, column=0, sticky="s")
+
+
+       #############################################
+        # Track Pool
+        #############################################
+
+        self.coll_list = ttk.Treeview(self.pool_frame)
+        self.coll_list.pack(fill="both", expand=1)
+
+        self.coll_list["columns"]=("artist", "id")
+        self.coll_list.column("#0", width=25,  minwidth=10)
+        self.coll_list.column("artist", width=30, minwidth=10)
+        self.coll_list.column("id", width=5,  minwidth=3)
+
+        self.coll_list.heading("#0",text="Release",anchor=tk.W)
+        self.coll_list.heading("artist", text="Artist", anchor=tk.W)
+        self.coll_list.heading("id", text="Discogs ID",anchor=tk.W)
+
+
+        # Display Frames
+
+        self.mix_info_frame.pack(fill="both", expand=1, side = "top")
+        self.buttons_frame.pack(side="bottom", fill="x")
+        self.pool_frame.pack(fill="both", expand=1, side = "bottom")
+
+
+    def insert_track_pool(self):
+        self.collection = models.Collection(self.conn)
+        try:
+            self.all_releases = self.collection.get_all_db_releases()
+            log.debug("GUI: Got all releases from Collection Model")
+        except:
+            log.error("GUI: Couldn't get all releases from Collection Model")
+        
+        for i, row in enumerate(self.all_releases):
+            self.coll_list.insert("" , i, text=row["discogs_title"], values=(row["d_artist"], row["discogs_id"]))
+
+
+    def save_mix(self):
+        pass
         
 
 
 
 class edit_track_info(widget_frame):
 
-    def __init__(self, parent):
+    def __init__(self, parent, conn):
         self.title = "Edit Track Info"
         super().__init__(parent, self.title)
         self.view_track_content()
+        self.conn = conn
+
 
 
     def view_track_content(self):
-        pass
+        self.track_info_frame = tk.LabelFrame(self.edit_win, text="Mix Info")
+        self.buttons_frame = tk.Frame(self.edit_win)
 
+        #############################################
+        # Track edit
+        #############################################
+
+        tk.Label(self.track_info_frame, text="Track Position").grid(row=0, column=0, sticky="w")
+        self.name_entry = tk.Entry(self.track_info_frame, width=30)
+        self.name_entry.grid(row=0, column=1, sticky="w")
+        try:
+            self.name_entry.insert(0, self.track_data["track_pos"])
+        except:
+            log.error("Couldn't insert Data")
+
+        tk.Label(self.track_info_frame, text="Discogs Title").grid(row=1, column=0, sticky="w")
+        self.played_entry = tk.Entry(self.track_info_frame, width=30)
+        self.played_entry.grid(row=1, column=1, sticky="w")
+        self.played_entry.insert(0, self.track_data["discogs_title"])
+
+        tk.Label(self.track_info_frame, text="Trans Rating").grid(row=2, column=0, sticky="w")
+        self.venue_entry = tk.Entry(self.track_info_frame, width=30)
+        self.venue_entry.grid(row=2, column=1, sticky="w")
+        self.venue_entry.insert(0, self.track_data["trans_rating"])
+
+        tk.Label(self.track_info_frame, text="Trans Notes").grid(row=3, column=0, sticky="w")
+        tk.Label(self.track_info_frame, text=self.track_data["trans_notes"]).grid(row=3, column=1, sticky="w")
+
+        tk.Label(self.track_info_frame, text="BPM").grid(row=4, column=0, sticky="w")
+        tk.Label(self.track_info_frame, text=self.track_data["bpm"]).grid(row=4, column=1, sticky="w")
+
+        tk.Label(self.track_info_frame, text="Key").grid(row=5, column=0, sticky="w")
+        tk.Label(self.track_info_frame, text=self.track_data["key"]).grid(row=4, column=1, sticky="w")
+
+        tk.Label(self.track_info_frame, text="Key Notes").grid(row=6, column=0, sticky="w")
+        tk.Label(self.track_info_frame, text=self.track_data["key_notes"]).grid(row=4, column=1, sticky="w")
+
+        for i in range(5,10):
+            self.track_info_frame.rowconfigure(i, weight=1)
+
+        self.update_mix_btn = tk.Button(self.track_info_frame, text="Update Mix", command=update_mix)
+        self.update_mix_btn.grid(row=8, column=0, sticky="s")
+
+
+
+        ###############################################
+
+        # Buttons Area
+
+        tk.Button(self.buttons_frame, text="Save Track Data").pack(side="left")
+        tk.Button(self.buttons_frame, text="Cancel").pack(side="right")
+
+        # Display Frames
+
+        self.mix_info_frame.pack(fill="both", expand=1, side = "top")
+        self.buttons_frame.pack(side="bottom", fill="x")
+    
+
+    def save_track_data(self):
+        pass
 
 
 
--- gui.py
+++ gui.py
@@ -1,12 +1,12 @@
 ################################
 # IMPORTS
-
 from discodos import views
 from discodos import ctrls
 from discodos import models
 from discodos import utils
 from discodos import log
 from discodos.widgets import edit_mix_view
+from discodos.widgets import add_mix_view
 from discodos.widgets import edit_track_info
 import os
 from pathlib import Path
@@ -24,19 +24,13 @@ class main_frame():
         self.main_win = tk.Tk()                           
         self.main_win.geometry("800x600")                # Fixed size for now, maybe scalable later
         # self.main_win.resizable(False, False) 
-        
-        
-        
-    
+
         self.main_win.title("Discodos") # EDIT: Add relevant Information to title, like Titles in Mix etc
 
         # Create all Widgets, outsourced to its own function
         self.create_widgets()
         self.mix_starter()
 
-        
-        
-        
                     
     # Exit GUI cleanly
     def _quit(self):
@@ -85,6 +79,8 @@ class main_frame():
                 log.error("GUI: Inserting Mix Row failed")
                 self.status.set("Inserting Mix Row failed")
 
+            
+
         # Get the highest number of every column-dataset of the dictionary
         # set every columnwdith equal to the highest number
 
@@ -93,36 +89,56 @@ class main_frame():
         try:
             start_child_id = self.mix_list.get_children()[0]
             self.mix_list.selection_set(start_child_id)
-            log.debug("GUI: Set Focus on first Item")
+            log.debug("GUI: Set Focus on first Mix Item")
+
         except:
-            log.error("GUI: Couldn't Set Focus on First Item")
-            self.status.set("Couldn't Set Focus on First Item")
+            log.error("GUI: Couldn't Set Focus on Mix Item")
+            self.status.set("Couldn't Set Focus on Mix Item")
+
+        
 
     
     def show_mix(self, event):
 
         curItem = self.mix_list.focus()
+
+        
+            
+
         try:
             mix = models.Mix(self.conn, self.mix_list.item(curItem,"text"))
             mix_data = mix.get_full_mix(verbose = True) 
             log.debug("GUI: Retrieved Mix data")   
-            self.status.set("Retrieved Mix data")   
+            self.status.set("Retrieved Mix data") 
+
         except: 
             log.error("GUI: Getting Mix Data failed")
             self.status.set("Getting Mix Data failed")  
+        
+        
+        ########################################################
 
-            # TODO
-            # Everything's working except RELEASE
-
-        if mix_data is not False:
+        if mix_data is not []:
+            
+            self.tracks_list.delete(*self.tracks_list.get_children())
             for i, row in enumerate(mix_data):
                 try:
-                    self.tracks_list.insert("" , i, text="", values=(row["d_artist"], row["d_track_name"], row["d_track_no"], row["key"], row["bpm"], row["key_notes"], row["trans_rating"], row["trans_notes"], row["notes"]))
+                    self.tracks_list.insert("", i, text=row["d_artist"] , values=(row["d_track_name"], row["d_track_no"], row["key"], row["bpm"], row["key_notes"], row["trans_rating"], row["trans_notes"], row["notes"]))
                     log.debug("GUI: Inserted Track row")
                     self.status.set("Inserted Track row")  
                 except:
                     log.error("GUI: Inserting Track Row failed")
                     self.status.set("Inserting Track Row failed") 
+                
+            try:
+                start_child_id_two = self.tracks_list.get_children()[0]
+                self.tracks_list.selection_set(start_child_id_two)
+                log.debug("GUI: Set Focus on first Track Item")
+
+            except:
+                log.error("GUI: Couldn't Set Focus on Track Item")
+                self.status.set("Couldn't Set Focus on Track Item")
+
         else:
             log.error("GUI: Mix Data is " + str(mix_data))
             self.status.set("GUI: Mix Data is " + str(mix_data)) 
@@ -130,27 +146,30 @@ class main_frame():
 
 
     def open_widget(self, view):
-        if view == "mix":
+
+        if view == "edit_mix":
             curItem = self.mix_list.focus()
             try:
-                mix = models.Mix(self.conn, self.mix_list.item(curItem,"text"))
+                mix = models.Mix(self.conn, self.mix_list.item(curItem,"text")) 
                 mix_data = mix.get_mix_info() 
                 log.debug("GUI: Retrieved Mix Info")   
                 self.status.set("Retrieved Mix Info") 
-
+                
                 # ############## # # # # # # # # # #
                 # TODO: Get Mix Edit Window to change info on selected list item in mix view
                 try:
                     if self.mix_edit_win.win_state == "normal" and curItem is self.mix_list.focus(): 
+                        log.debug("GUI: Focused Mix Edit Window") 
                         self.mix_edit_win.edit_win.focus()
 
                     elif self.mix_edit_win.win_state == "normal" and curItem is not self.mix_list.focus():
+                        log.debug("GUI: Reloaded Mix Edit Window") 
                         self.mix_edit_win._quit()
 
                         try:
                             mix = models.Mix(self.conn, self.mix_list.item(self.mix_list.focus(),"text"))
                             mix_data = mix.get_mix_info() 
-                            log.debug("GUI: Got Mix data again")
+                            log.debug("GUI: Got Mix data again") 
 
                         except:
                             log.error("GUI: Couldn't get mix data again")
@@ -168,9 +187,66 @@ class main_frame():
                 log.error("GUI: Getting Mix Data failed")
                 self.status.set("Getting Mix Data failed")  
 
+        
+        elif view == "add_mix":
+            try:
+                if self.add_mix_win.win_state == "normal":
+                    self.add_mix_win.edit_win.focus()
+                    log.debug("GUI: Mix Window State is " + self.add_mix_win.win_state)
+
+                else:
+                    self.add_mix_win = add_mix_view(self.main_win, self.conn)
+                    self.add_mix_win.edit_win.focus()
             
-        elif view == "track":
-            self.track_edit_win = edit_track_info(self.main_win)
+            except:
+                self.add_mix_win = add_mix_view(self.main_win, self.conn)
+                self.add_mix_win.edit_win.focus()
+
+            
+        elif view == "track_edit":
+
+            curItem = self.tracks_list.focus()
+            try:
+                track = models.Mix(self.conn, self.tracks_list.item(curItem,"d_release_id")) 
+                track_data = track.get_one_mix_track() 
+                log.debug("GUI: Retrieved Track Info")   
+                self.status.set("Retrieved Track Info") 
+                
+                # ############## # # # # # # # # # #
+                try:
+                    if self.track_edit_win.win_state == "normal" and curItem is self.tracks_list.focus(): 
+                        log.debug("GUI: Focused Track Edit Window") 
+                        self.track_edit_win.edit_win.focus()
+
+                    elif self.track_edit_win.win_state == "normal" and curItem is not self.track_list.focus():
+                        log.debug("GUI: Reloaded Track Edit Window") 
+                        self.track_edit_win._quit()
+
+                        try:
+                            track = models.Mix(self.conn, self.tracks_list.item(self.tracks_list.focus()," ")) # # START HERE
+
+                        # Fetch Mix data with Mix ID
+                        # THEN fetch get_one_mix with track Id
+                        # what is track_id?
+
+                            track_data = track.get_one_mix_track() 
+                            log.debug("GUI: Got Track data again") 
+
+                        except:
+                            log.error("GUI: Couldn't get Track data again")
+
+                        self.track_edit_win = edit_track_info(self.main_win, track_data, self.conn)
+                        self.track_edit_win.edit_win.focus()
+
+                    log.debug("GUI: Track Window State is " + self.track_edit_win.win_state)
+
+                except:
+                    self.track_edit_win = edit_track_info(self.main_win, track_data, self.conn)  
+                    log.debug("GUI: Track Window State is " + self.track_edit_win.win_state) 
+
+            except: 
+                log.error("GUI: Getting Track Data failed")
+                self.status.set("Getting Track Data failed")  
 
 
     #####################################################################################    
@@ -209,7 +285,7 @@ class main_frame():
         self.tracks_list = ttk.Treeview(self.tracks_frame)
         self.tracks_list.pack(fill="both", expand=1)
 
-        self.tracks_list["columns"]=("artist", "track", "trackpos", "key", "bpm", "keynotes", "transr", "transnotes", "tracknotes")
+        self.tracks_list["columns"]=("artist", "track", "trackpos", "key", "bpm", "keynotes", "transr", "transnotes", "d_release_id")
         self.tracks_list.column("#0", width=20,  minwidth=10)
         self.tracks_list.column("artist", width=20, minwidth=10)
         self.tracks_list.column("track", width=20,  minwidth=10)
@@ -219,11 +295,10 @@ class main_frame():
         self.tracks_list.column("keynotes", width=10, minwidth=5)
         self.tracks_list.column("transr", width=10, minwidth=5)
         self.tracks_list.column("transnotes", width=10, minwidth=5)
-        self.tracks_list.column("tracknotes", width=20, minwidth=10)
+        self.tracks_list.column("d_release_id", width=20, minwidth=10)
 
 
-        self.tracks_list.heading("#0",text="Release",anchor=tk.W)
-        self.tracks_list.heading("artist", text="Artist", anchor=tk.W)
+        self.tracks_list.heading("#0", text="Artist", anchor=tk.W)
         self.tracks_list.heading("track", text="Track Name",anchor=tk.W)
         self.tracks_list.heading("trackpos", text="Track Pos",anchor=tk.W)
         self.tracks_list.heading("key", text="Key",anchor=tk.W)
@@ -231,7 +306,7 @@ class main_frame():
         self.tracks_list.heading("keynotes", text="Key Notes",anchor=tk.W)
         self.tracks_list.heading("transr", text="Trans. Rating",anchor=tk.W)
         self.tracks_list.heading("transnotes", text="Trans. Notes",anchor=tk.W)
-        self.tracks_list.heading("tracknotes", text="Track Notes",anchor=tk.W)
+        self.tracks_list.heading("d_release_id", text="Mix Track ID",anchor=tk.W) 
 
         #########################################################################
 
@@ -249,10 +324,10 @@ class main_frame():
 
         self.btn_frame = tk.Frame(self.main_win)
         
-        self.new_mix_btn = tk.Button(self.btn_frame, text="New Mix")
-        self.edit_mix_btn = tk.Button(self.btn_frame, text="Edit Mix", command=lambda: self.open_widget("mix"))
+        self.new_mix_btn = tk.Button(self.btn_frame, text="New Mix", command=lambda: self.open_widget("add_mix"))
+        self.edit_mix_btn = tk.Button(self.btn_frame, text="Edit Mix", command=lambda: self.open_widget("edit_mix"))
         self.del_mix_btn = tk.Button(self.btn_frame, text="Delete Mix")
-        self.edit_track_btn = tk.Button(self.btn_frame, text="Edit Track Info", command=lambda: self.open_widget("track"))
+        self.edit_track_btn = tk.Button(self.btn_frame, text="Edit Track Info", command=lambda: self.open_widget("track_edit"))
         self.rmv_track_btn = tk.Button(self.btn_frame, text="Remove Track")
         
 
