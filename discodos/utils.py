7ba79ab4709021443356c61395d37ca490118fe2
J0J0 T <jojo@peek-a-boo.at>
2020-02-02 12:45:50 +0100

    utils.py Config: also fix quotes in posix disco wrapper

--- discodos/utils.py
+++ discodos/utils.py
@@ -92,7 +92,7 @@ class Config():
             script_contents = '#!/bin/bash\n'
             script_contents+= '# This is the DiscoDOS cli wrapper.\n'
             script_contents+= 'source "{}"\n'.format(venv_act)
-            script_contents+= '{} "$@"\n'.format(self.discodos_root / 'cli.py')
+            script_contents+= '"{}" "$@"\n'.format(self.discodos_root / 'cli.py')
             sysinst = self.discodos_root / 'install_cli_system.sh'
             sysinst_contents = 'sudo -p "Need your users password to allow '
             sysinst_contents+= 'systemwide installation of disco cli command: " '
