821d4292974f6a63c06f59167b119f51e5fa5dac
J0J0 T <jojo@peek-a-boo.at>
2020-04-30 20:02:38 +0200

    posix_pack.sh: add --clean option, remove distpath

--- posix_pack.sh
+++ posix_pack.sh
@@ -10,10 +10,18 @@ fi
 
 source ~/.venvs/discodos_pack/bin/activate
 
-pyinstaller cli.py --onefile --name cli --distpath discodos --clean -y -p ~/.venvs/discodos_pack/lib/python3.7/site-packages/ -p ~/.venvs/discodos_pack/src/discogs-client/
+pyinstaller cli.py --onefile --name cli --clean -y -p ~/.venvs/discodos_pack/lib/python3.7/site-packages/ -p ~/.venvs/discodos_pack/src/discogs-client/
 
-pyinstaller setup.py --onefile --name setup --distpath discodos --clean -y -p ~/.venvs/discodos_pack/lib/python3.7/site-packages/ -p ~/.venvs/discodos_pack/src/discogs-client/
+pyinstaller setup.py --onefile --name setup --clean -y -p ~/.venvs/discodos_pack/lib/python3.7/site-packages/ -p ~/.venvs/discodos_pack/src/discogs-client/
 
-pyinstaller sync.py --onefile --name sync --distpath discodos --clean -y -p ~/.venvs/discodos_pack/lib/python3.7/site-packages/ -p ~/.venvs/discodos_pack/src/discogs-client/
+pyinstaller sync.py --onefile --name sync --clean -y -p ~/.venvs/discodos_pack/lib/python3.7/site-packages/ -p ~/.venvs/discodos_pack/src/discogs-client/
+
+if [[ $1 == '--clean' ]]; then
+    rm -rf discodos
+    mv -v dist discodos
+else
+    rmdir discodos
+    mv dist/* discodos/
+fi
 
 tar -zcvf discodos-${VERSION}-${OS}.tar.gz discodos
