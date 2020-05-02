a1ebb23ffd612986a3bfa8087052f0690af6e6f7
J0J0 T <jojo@peek-a-boo.at>
2020-05-02 20:31:06 +0200

    win_pack.bat: add config.yaml to package

--- win_pack.bat
+++ win_pack.bat
@@ -7,12 +7,16 @@ pyinstaller cli.py --onefile --clean -y
 pyinstaller setup.py --name winconfig --onefile --clean -y
 pyinstaller sync.py --onefile --clean -y
 
+echo Running _once_ to create config.yaml
+dist\cli.exe
+
 echo Copying to discodos dir...
 cd dist
 md discodos
 copy /y cli.exe discodos
 copy /y winconfig.exe discodos
 copy /y sync.exe discodos
+copy /y config.yaml discodos
 
 echo Zipping....
 zip -r discodos.zip discodos
