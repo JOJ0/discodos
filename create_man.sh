96b2762296f34cfd3a38e01b8ddbb5365a333861
J0J0 T <jojo@peek-a-boo.at>
2020-06-15 20:36:47 +0200

    create_man.sh: fix all subcommand with their own include file and fix ordering of options

--- create_man.sh
+++ create_man.sh
@@ -9,32 +9,32 @@ function man_disco_mix() {
 
 }
 function man_disco_suggest() {
-    cli2man -i man/disco_suggest_help.txt --os DiscoDOS --info-section "arguments" --set-order "NAME,SYNOPSIS,DESCRIPTION,ARGUMENTS,OPTIONS,AUTHORS,SEE ALSO" --include man/disco_suggest_add.mdoc --see-also disco,disco-mix,disco-import,disco-search,discosync $1
+    cli2man -i man/disco_suggest_help.txt --os DiscoDOS --info-section "arguments" --set-order "NAME,SYNOPSIS,DESCRIPTION,ARGUMENTS,OPTIONS,AUTHORS,SEE ALSO" --see-also disco,disco-mix,disco-import,disco-search,discosync --include man/disco_suggest_add.mdoc $1
 }
 function man_disco_import() {
-    cli2man -i man/disco_import_help.txt --see-also disco,disco-mix,disco-suggest,disco-search --os DiscoDOS --include man/disco_sub_add.mdoc $1
+    cli2man -i man/disco_import_help.txt --os DiscoDOS --info-section "arguments" --set-order "NAME,SYNOPSIS,DESCRIPTION,ARGUMENTS,OPTIONS,AUTHORS,SEE ALSO" --see-also disco,disco-mix,disco-suggest,disco-search,discosync --include man/disco_import_add.mdoc $1
 }
 function man_disco_search() {
-    cli2man -i man/disco_search_help.txt --see-also disco,disco-mix,disco-suggest,disco-import --os DiscoDOS --include man/disco_sub_add.mdoc $1
+    cli2man -i man/disco_search_help.txt --os DiscoDOS --info-section "arguments" --set-order "NAME,SYNOPSIS,DESCRIPTION,ARGUMENTS,OPTIONS,AUTHORS,SEE ALSO" --see-also disco,disco-mix,disco-suggest,disco-import,discosync --include man/disco_search_add.mdoc $1
 }
 function man_discosync() {
-    cli2man -i man/discosync_help.txt --see-also disco,disco-mix,disco-suggest,disco-import,disco-search --os DiscoDOS --include man/discosync_add.mdoc $1
+    cli2man -i man/discosync_help.txt --os DiscoDOS --see-also disco,disco-mix,disco-suggest,disco-import,disco-search --include man/discosync_add.mdoc $1
 }
 
 if [ "$1" == 'doit' ]; then
     #man_disco > man/disco.mdoc
-    man_disco_mix > man/disco-mix.mdoc
+    #man_disco_mix > man/disco-mix.mdoc
     #man_disco_suggest > man/disco-suggest.mdoc
-    #man_disco_import > man/disco-import.mdoc
+    man_disco_import > man/disco-import.mdoc
     #man_disco_search > man/disco-search.mdoc
     #man_discosync > man/discosync.mdoc
     echo "5 files written to man/"
     echo "Now fix stuff manually"
 else
     #man_disco -m
-    man_disco_mix -m
+    #man_disco_mix -m
     #man_disco_suggest -m
-    #man_disco_import -m
+    man_disco_import -m
     #man_disco_search -m
     #man_discosync -m
 fi
