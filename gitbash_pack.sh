0947b7083319c847bddfcf693223452803c45ab9
J0J0 T <jt@peek-a-boo.at>
2020-06-19 14:43:44 +0200

    add gitbash_pack.sh (currently tabulate issue though)

---
+++ gitbash_pack.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# package for windows running git-bash
+echo Activating Python venv
+. "$HOME/.venvs/discodos_fresh/Scripts/activate"
+
+
+VERSION='1.0_rc2'
+
+if [[ $1 == '--clean' ]]; then
+    rm -rf dist
+fi
+
+echo Packaging disco...
+pyinstaller discodos/cmd/cli.py --name disco --onefile --clean -y -p ~/.venvs/discodos_fresh/Lib/site-packages/ -p  ~/.venvs/discodos_fresh/src/python3-discogs-client/
+echo Packaging discosync...
+pyinstaller discodos/cmd/sync.py --name discosync --onefile --clean -y -p ~/.venvs/discodos_fresh/Lib/site-packages/ -p  ~/.venvs/discodos_fresh/src/python3-discogs-client/
+
+echo Running _once_ to create config.yaml...
+dist/disco.exe
+
+echo Copying to discodos dir...
+cd dist
+mkdir discodos
+cp disco.exe discodos
+cp discosync.exe discodos
+cp config.yaml discodos
+
+echo Zipping...
+ZIPNAME=discodos-${VERSION}-Win.zip
+$HOME/bin/zip -r $ZIPNAME discodos
+
+cd ..
+mv dist/${ZIPNAME} .
+
