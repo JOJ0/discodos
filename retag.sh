66c7ad97e66308015249290905f62983d3e657b5
J0J0 T <jojo@peek-a-boo.at>
2021-03-02 15:49:55 +0100

    retag.sh: push options via $2

--- retag.sh
+++ retag.sh
@@ -4,8 +4,9 @@ if [[ -z $1 ]]; then
     echo "usage: ./retag.sh <version>  # omit v prefix"
     exit 1
 fi
+PUSH_OPTS="$2"
 
 set -x
 VERS=$1
 
-git tag -d v$VERS; git push origin --delete v$VERS; git tag v$VERS; git push --force-with-lease; git push --tags
+git tag -d v$VERS; git push origin --delete v$VERS; git tag v$VERS; git push $PUSH_OPTS; git push --tags
