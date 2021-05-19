9917dafb5610d54a067ddaf6dfff7c7a8a70b470
D8bp8Ags <83901401+D8bp8Ags@users.noreply.github.com>
2021-05-19 23:36:14 +0200

    move MainWindow files to discodos/qt folder

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import sys
 
-from MainWindow import Ui_MainWindow
+from qt.MainWindow import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt
 from discodos.models import Mix, log, Collection
