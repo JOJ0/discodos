b6d71c0772a7927e8813b19a8519bebf9c902de4
J0J0 T <jojo@peek-a-boo.at>
2021-08-04 07:58:05 +0200

    test_collection: add test_stats_tracks_total

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -390,6 +390,15 @@ class TestCollection(unittest.TestCase):
         self.assertEqual(db_return[0], 4)  # should be 4 releases in coll.
         print("{} - {} - END".format(self.clname, name))
 
+    def test_stats_tracks_total(self):
+        name = inspect.currentframe().f_code.co_name
+        print("\n{} - {} - BEGIN".format(self.clname, name))
+        self.collection = Collection(False, self.db_path)
+        db_return = self.collection.stats_tracks_total()
+        # self.debug_db(db_return)
+        self.assertEqual(db_return[0], 5)  # should be 5 tracks in coll.
+        print("{} - {} - END".format(self.clname, name))
+
     @classmethod
     def tearDownClass(self):
         name = inspect.currentframe().f_code.co_name
