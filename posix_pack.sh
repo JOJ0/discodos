f00e2fc6b72127cf8f5db85583685abab8bbc322
J0J0 T <jojo@peek-a-boo.at>
2022-04-02 16:34:29 +0200

    Bump version: 1.0.1 → 1.1

--- posix_pack.sh
+++ posix_pack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION='1.0.1'
+VERSION='1.1.0'
 
 if [[ $OSTYPE == "darwin"* ]]; then
     OS="macOS"
--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -29,7 +29,7 @@ author = 'J0J0 Todos'
 version = '1.0'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.1'
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
