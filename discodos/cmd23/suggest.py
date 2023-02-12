5963d604d00cc56fad80b776a9d2edeff6d9c6b6
J0J0 Todos <jojo@peek-a-boo.at>
2023-02-12 09:33:28 +0100

    Add args and opts to cmd23/suggest.py

--- discodos/cmd23/suggest.py
+++ discodos/cmd23/suggest.py
@@ -2,9 +2,18 @@ import click
 
 
 @click.command(name='suggest')
-@click.argument('name', nargs=1)
-def suggest_cmd(name):
+@click.argument('suggest_search', nargs=1, metavar='SEARCH_TERMS', default=0)
+@click.option(
+    "-b", "--bpm", 'suggest_bpm', type=int, metavar="BPM",
+    help='''suggests tracks based on BPM value, within a
+    pitch-range of +/-6 percent.''')
+@click.option(
+    "-k", "--key",'suggest_key', type=str, metavar="KEY",
+    help='suggests tracks based on musical key.')
+@click.pass_obj
+def suggest_cmd(helper, suggest_search, suggest_bpm, suggest_key):
     """
-    Simple command that says hello
+    Suggests tracks based on what you've played before, views tracks
+    within a BPM range or sharing a musical key.
     """
-    click.echo(f'Hello {name}')
+    click.echo(f'Hello {suggest_search}')
