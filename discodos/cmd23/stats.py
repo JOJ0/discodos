c42d8f3f9aa6e011e772aa58dc2f37fe69397c68
J0J0 Todos <jojo@peek-a-boo.at>
2023-02-12 09:08:06 +0100

    Add args and opts to cmd23/stats.py

--- discodos/cmd23/stats.py
+++ discodos/cmd23/stats.py
@@ -2,9 +2,13 @@ import click
 
 
 @click.command(name='stats')
-@click.argument('name', nargs=1)
-def stats_cmd(name):
+@click.pass_obj
+def stats_cmd(helper):
     """
-    Simple command that says hello
+    Displays statistics about the collection.
+
+    Besides showing stats like the size of the collection (on Discogs vs. in
+    the DiscoBASE) also counts on existing mixes, how many tracks they contain
+    and which additional metadata is present, are availalbe.
     """
-    click.echo(f'Hello {name}')
+    click.echo(f'Hello')
