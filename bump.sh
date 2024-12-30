a47b04f8753e0ae0e6ba83e0021248b6dd91d566
J0J0 Todos <jojo@peek-a-boo.at>
2024-12-30 23:40:32 +0100

    Fix bump.sh version prefix

--- bump.sh
+++ bump.sh
@@ -15,6 +15,6 @@ else
     echo -e "\nTHIS IS A DRY-RUN\n"
 fi
 
-$DRY git tag -d v$VERS
-$DRY git push origin --delete v$VERS
+$DRY git tag -d $VERS
+$DRY git push origin --delete $VERS
 $DRY bump2version build --new-version $VERS --commit --tag --verbose
