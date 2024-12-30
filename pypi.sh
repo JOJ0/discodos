0852c4c5a3de0a4e61d2b6a94c22a3bd95ad23b8
J0J0 Todos <jojo@peek-a-boo.at>
2024-12-31 00:12:45 +0100

    Add pypi.sh release helper

---
+++ pypi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+python -m pip install --upgrade pip
+python -m pip install --upgrade build twine
+echo
+echo "Cleaning up dist directory."
+echo
+rm -rf dist/*
+echo
+python -m build
+python -m twine upload --repository testpypi dist/*
+echo
+echo "Test install:"
+echo "python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps discodos"
+echo
+echo "All good? Upload to real PyPI:"
+echo python3 -m twine upload dist/*
+echo
+
