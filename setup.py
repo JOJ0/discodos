49671f9b6f4e2c8270fa5b7a5b4362a69175954b
J0J0 Todos <jojo@peek-a-boo.at>
2024-12-31 10:18:35 +0100

    Get rid of pbr (was requirement for Debian package)
    and use a regular setup.py.

--- setup.py
+++ setup.py
@@ -1,8 +1,47 @@
-#!/usr/bin/env python
-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    setup_requires=['pbr'],
-    pbr=True,
+    name="discodos",
+    version="3.0.0",
+    description="DJ and record collector's toolbox for Discogs",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    author="J0J0 Todos",
+    url="https://discodos.jojotodos.net",
+    project_urls={
+        "Bug Tracker": "https://github.com/joj0/discodos/issues",
+        "Documentation": "https://discodos.readthedocs.io",
+        "Source Code": "https://github.com/joj0/discodos/",
+    },
+    packages=find_packages(where=".", include=["discodos"]),
+    test_suite="tests",
+    license="GPLv3+",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Environment :: Console",
+    ],
+    entry_points={
+        "console_scripts": [
+            "discosync = discodos.cmd.sync:main",
+            "dsc = discodos.cmd23:main_cmd",
+        ],
+    },
+    flake8_ignore=["E501", "E225"],
+    exclude=[
+        ".git",
+        "__pycache__",
+        "docs/source/conf.py",
+        "old",
+        "build",
+        "dist",
+        ".eggs",
+        "discodos_logo.py",
+    ],
 )
+
